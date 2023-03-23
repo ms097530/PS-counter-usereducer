16297c8e308762938c33148534b39cd22b7c2197
Michael Schultz <mikewschultz9029@gmail.com>
2023-03-23 10:24:58 -0400

    added jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,9 @@ import Counter from "../components/Counter"
 export default function HomePage()
 {
     return (
-        <Counter />
+        <div>
+            <h2>Home Page</h2>
+            <Counter />
+        </div>
     )
 }
